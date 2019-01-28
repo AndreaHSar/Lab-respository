630d610890fae8a4c1e46316f5642325b48a0c56
AndreaHSar <47091996+AndreaHSar@users.noreply.github.com>
2019-01-27 23:00:11 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+const cajaAzul = document.getElementById('caja-azul');
+
+//cajaAzul.style.display = 'none';
+
+cajaAzul.addEventListener('click', function (){
+  cajaAzul.style.display = 'none';
+});
